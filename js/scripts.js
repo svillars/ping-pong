afe3fcdd7d5df6ea6d33b29f2d3c1d8371edbf9a
Shane Villars <shanevillars@yahoo.com>
2017-01-20 09:39:33 -0800

    add functionality that replaces numbers in range to 'ping' if divisible by 3

--- js/scripts.js
+++ js/scripts.js
@@ -14,12 +14,15 @@ var output = [];
 function pingPong(num) {
   for(i = 1; i <= num; i++) {
     if (i % 15 === 0) {
-    output.push("ping-pong");
-  } else {
-    output.push(i);
-  }
-    console.log(num);
-    console.log(output);
-    output.join(',');
+      output.push("ping-pong");
+    } else if (i % 3 === 0) {
+        output.push("ping");
+    } else {
+        output.push(i);
+    }
+
+        console.log(num);
+        console.log(output);
+        output.join(',');
   }
 }
