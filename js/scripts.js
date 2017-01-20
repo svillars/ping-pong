b2aeb19b3a2163bbf60d98343f4ae5b6d587ccfd
Shane Villars <shanevillars@yahoo.com>
2017-01-20 10:03:42 -0800

    add functionality that replaces numbers in range to 'pong' if divisible by 5

--- js/scripts.js
+++ js/scripts.js
@@ -3,26 +3,26 @@ $(document).ready(function() {
   $("form").submit(function(event) {
     event.preventDefault();
 
+    $(".output").text(" ");
     var num = $("input").val();
     pingPong(num);
-    $(".output").text(output);
+    $(".output").text(final);
   })
 })
 
 //Business Logic =================================
-var output = [];
 function pingPong(num) {
+  var output = [];
   for(i = 1; i <= num; i++) {
     if (i % 15 === 0) {
       output.push("ping-pong");
     } else if (i % 3 === 0) {
-        output.push("ping");
+      output.push("ping");
+    } else if (i % 5 === 0) {
+      output.push("pong");
     } else {
-        output.push(i);
+      output.push(i);
     }
-
-        console.log(num);
-        console.log(output);
-        output.join(',');
+    final = output.join(',');
   }
 }
